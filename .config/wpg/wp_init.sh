cddcbf2d57f13d53b3af3303597b2de0e837e764
yuk1odesu <prozh30@gmail.com>
2025-12-06 13:05:33 +0300

    added wallpapers and updated .config - now waybar, waypaper and mako configs work

--- .config/wpg/wp_init.sh
+++ .config/wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -nrs '/home/yuk1o/Pictures/wallpapers/wallhaven-6dx3mw.jpg' '/home/yuk1o/Pictures/wallpapers/wallhaven-6dx3mw.jpg'
+wpg -nrs '/home/yuk1o/Pictures/wallpapers/mikuangel.jpg' '/home/yuk1o/Pictures/wallpapers/mikuangel.jpg'
